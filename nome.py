42f5d67c31f13b8dc6e54921732d0944dbb20f76
Armando Martins <armando.amds97@gmail.com>
2025-05-06 11:29:23 -0300

    começando POO em python

---
+++ nome.py
@@ -0,0 +1,10 @@
+from biblioteca import Pessoa
+
+aluno01 = Pessoa("Armando", 27, 100)
+aluno02 = Pessoa("Guilherme", 21, 82)
+#print(f"{aluno01.nome} tem {aluno01.idade} anos e pesa {aluno01.peso} kgs")
+#print(f"{aluno02.nome} tem {aluno01.idade} anos e pesa {aluno01.peso} kgs")
+#aluno02.nome = "Guilherme o grande"
+#print(f"{aluno02.nome} tem {aluno02.idade} anos e pesa {aluno02.peso} kgs")
+
+aluno02.comer("")
